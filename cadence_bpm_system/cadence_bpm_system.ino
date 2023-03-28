7aa7003d81b2718813433fceec392d45184329a2
Kyle Eaton <keaton12@Live.com>
2023-03-28 11:23:02 -0500

    WiFi and loop delay.
    Connect to WiFi in the setup function and define a delay in the loop (currently 10 seconds). The delay will help with development.

--- cadence_bpm_system/cadence_bpm_system.ino
+++ cadence_bpm_system/cadence_bpm_system.ino
@@ -1,9 +1,35 @@
-void setup() {
-  // put your setup code here, to run once:
 
+#include <WiFi.h>
+#include <time.h>
+
+const char* ssid = "";
+const char* password = "";
+WiFiClient client;  // TODO: WiFiClientSecure instead?
+
+unsigned long prev_timestamp;
+unsigned int loop_delay = 10000;  // 10 secs
+
+void setup() {
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+    Serial.println("");
+    Serial.println("WiFi connected");
+
+    prev_timestamp = millis();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  unsigned long curr_timestamp = millis();
+  if(curr_timestamp - prev_timestamp > loop_delay) {
 
+
+
+
+    prev_timestamp = millis();
+  }
 }
