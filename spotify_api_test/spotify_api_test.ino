108de1fc3ab213d9018f1dc14400bb2b1a9bf89a
Sophie Stephenson <sophia.r.stephenson@gmail.com>
2023-04-22 14:15:00 -0500

    try new spotify stuff

--- spotify_api_test/spotify_api_test.ino
+++ spotify_api_test/spotify_api_test.ino
@@ -1,6 +1,12 @@
 /*******************************************************************
-    Prints song information from a Spotify search query to the
-    serial monitor using an ES32 or ESP8266
+    Allow you to specify a track/artist/album to play.
+
+    This is based on the request described here on the
+    spotify documentation:
+    https://developer.spotify.com/documentation/web-api/reference/player/start-a-users-playback/
+
+    This example shows some ways of invoking the above, but read the documentation
+    for full details.
 
     NOTE: You need to get a Refresh token to use this example
     Use the getRefreshToken example to get it.
@@ -22,15 +28,18 @@
     YouTube: https://www.youtube.com/brianlough
     Tindie: https://www.tindie.com/stores/brianlough/
     Twitter: https://twitter.com/witnessmenow
-
-    Search functionality by Jeremiah Ukwela
  *******************************************************************/
 
 // ----------------------------
 // Standard Libraries
 // ----------------------------
 
+#if defined(ESP8266)
+#include <ESP8266WiFi.h>
+#elif defined(ESP32)
 #include <WiFi.h>
+#endif
+
 #include <WiFiClientSecure.h>
 
 // ----------------------------
@@ -38,18 +47,29 @@
 // ----------------------------
 
 #include <SpotifyArduino.h>
-#include <SpotifyArduinoCert.h>
-#include <ArduinoJson.h>
+// Library for connecting to the Spotify API
 
+// Install from Github
+// https://github.com/witnessmenow/spotify-api-arduino
+
+// including a "spotify_server_cert" variable
+// header is included as part of the SpotifyArduino libary
+#include <SpotifyArduinoCert.h>
+
+#include <ArduinoJson.h>
 #include "arduino_secrets.h"
+// Library used for parsing Json from the API responses
+
+// Search for "Arduino Json" in the Arduino Library manager
+// https://github.com/bblanchon/ArduinoJson
 
 //------- Replace the following! ------
 
-char ssid[] = WIFI_SSID;
-char password[] = WIFI_PASS;
+char ssid[] = WIFI_SSID;         // your network SSID (name)
+char password[] = WIFI_PASS; // your network password
 
-char clientId[] = CLIENT_ID;
-char clientSecret[] = CLIENT_SECRET;
+char clientId[] = CLIENT_ID;     // Your client ID of your spotify APP
+char clientSecret[] = CLIENT_SECRET; // Your client Secret of your spotify APP (Do Not share this!)
 
 // Country code, including this is advisable
 #define SPOTIFY_MARKET "US"
@@ -59,112 +79,63 @@ char clientSecret[] = CLIENT_SECRET;
 WiFiClientSecure client;
 SpotifyArduino spotify(client, clientId, clientSecret, SPOTIFY_REFRESH_TOKEN);
 
-unsigned long delayBetweenRequests = 60000; // Time between requests (1 minute)
-unsigned long requestDueTime;               //time when request due
-
-#define MAX_RESULTS 2
+void playSingleTrack()
+{
+    char sampleTrack[] = "spotify:track:4uLU6hMCjMI75M1A2tKUQC";
+    char *deviceId = "dd1edf2aa262ca8250b03a0a8401ee387e4f80c4";
+    char body[200];
+    sprintf(body, "{\"uris\" : [\"%s\"]}", sampleTrack);
+    Serial.println(sizeof(body));
+    Serial.println(sizeof(deviceId));
+    spotify.playAdvanced(body, deviceId);
+}
 
 void setup()
 {
 
-  Serial.begin(115200);
+    Serial.begin(115200);
 
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  Serial.println("");
+    // Set WiFi to station mode and disconnect from an AP if it was Previously
+    // connected
+    WiFi.mode(WIFI_STA);
+    WiFi.disconnect();
+    delay(100);
 
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-
-  // Handle HTTPS Verification
-  client.setCACert(spotify_server_cert);
-  // ... or don't!
-  //client.setInsecure();
-
-  // If you want to enable some extra debugging
-  // uncomment the "#define SPOTIFY_DEBUG" in SpotifyArduino.h
-
-  spotify.searchDetailsBufferSize = 5000; //can be adjusted, needs about 500 per result, defaults to 3000
-
-  Serial.println("Refreshing Access Tokens");
-  if (!spotify.refreshAccessToken())
-  {
-    Serial.println("Failed to get access tokens");
-  }
-}
-
-bool getResultsCallback(SearchResult result, int index, int numResults)
-{
-
-  Serial.println("--------- Song Details ---------");
-
-  Serial.print("Song Index: ");
-  Serial.println(index);
-
-  Serial.print("Song ID: ");
-  Serial.println(result.trackUri);
-
-  Serial.print("Song Name: ");
-  Serial.println(result.trackName);
-
-  Serial.print("Song Artists: ");
-  for (int i = 0; i < result.numArtists; i++){
-    Serial.println(result.artists[i].artistName);
-  }
-
-  Serial.print("Song Album: ");
-  Serial.println(result.albumName);
-
-  Serial.print("Song Images (url): ");
-  for (int i = 0; i < result.numImages; i++){
-    Serial.println(result.albumImages[i].url);
-  }
-
-  Serial.println("-------------------------------");
-
-  return true;
-}
-
-void loop()
-{
-  if (millis() > requestDueTime)
-  {
-    Serial.print("Free Heap: ");
-    Serial.println(ESP.getFreeHeap());
-
-    Serial.println("Making Search Request:");
-
-    String query = "/?q=artist:Toto&type=track&market=US&offset=1";
-    
-    SearchResult results[MAX_RESULTS];
-    int status = spotify.searchForSong(query, MAX_RESULTS, getResultsCallback, results);
-    //spotify.searchForSong(query, limit, callback, array);
-    
-    if (status == 200)
+    // Attempt to connect to Wifi network:
+    Serial.print("Connecting Wifi: ");
+    Serial.println(ssid);
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED)
     {
-      Serial.println("Successfully got results: printing information");
-      for (int i = 0; i < MAX_RESULTS; i++)
-      {
-        //Plays 30 seconds of each song from the search
-        char body[100];
-        sprintf(body, "{\"uris\" : [\"%s\"]}", results[i].trackUri);
-        if (spotify.playAdvanced(body))
-        {
-            Serial.println("sent!");
-        }
-        delay(30*1e3);
-      }
+        Serial.print(".");
+        delay(500);
+    }
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    IPAddress ip = WiFi.localIP();
+    Serial.println(ip);
+
+    client.setCACert(spotify_server_cert);
+    // ... or don't!
+    //client.setInsecure();
+
+    // If you want to enable some extra debugging
+    // uncomment the "#define SPOTIFY_DEBUG" in SpotifyArduino.h
+
+    Serial.println("Refreshing Access Tokens");
+    if (!spotify.refreshAccessToken())
+    {
+        Serial.println("Failed to get access tokens");
+        return;
     }
 
-    requestDueTime = millis() + delayBetweenRequests;
-  }
+    delay(100);
+    Serial.println("Playing Single Track");
+    playSingleTrack();
+}
+
+// Example code is at end of setup
+void loop()
+{
 }
